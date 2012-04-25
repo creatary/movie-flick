d541b7d0f8f00560236b992e494a626696b4c77c
Krzysztof Nowicki <knowicki@knowicki-Latitude-E6520.(none)>
2012-04-25 17:29:15 +0200

    add small change for testing

--- movie-flick.rb
+++ movie-flick.rb
@@ -1,27 +1,39 @@
 require 'sinatra'
 
+
+from_user = ""
+to_app = ""
+body = ""
+
 get '/hi' do
-  "Hello World!"
+  "Hello World" 
 end
 
-get '/authorized' do
+get '/sms' do
+  "from user: #{from_user}, to app: #{to_app}, body: #{body}" 
+end
+
+get '/creatary/authorized' do
 # should return the URL to which Creatary should redirect
   if params['user'] == nil || params['session'] == nil
     "Missing params"
+  else
+    user = params['user']
   end
 end
 
-get 'denied' do
+get '/creatary/denied' do
 # should return the URL to which Creatary should redirect
 
 end
 
-get 'receive_sms' do
-  # |from_user, to_app, body|
-      
+get '/creatary/receive_sms' do
+  from_user = params['from_user']
+  from_user = params['to_app']
+  from_user = params['body']
 end
 
-get '/lifecycle_notification' do
+get '/creatary/lifecycle_notification' do
   # |channel, invoker, reason, application_name, notification_type, access_tokens|
 
 end
