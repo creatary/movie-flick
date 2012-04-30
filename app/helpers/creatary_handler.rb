26e24722e8c7804d748d6a2ce9c86276b8114e0d
Krzysztof Nowicki <knowicki@knowicki-Latitude-E6520.(none)>
2012-04-30 13:48:45 +0200

    replace LOGGER by Rails.logger

--- app/helpers/creatary_handler.rb
+++ app/helpers/creatary_handler.rb
@@ -9,7 +9,7 @@ class CreataryHandler
   end
 
   def receive_sms(from_user, to_app, body, transaction_id)
-    LOGGER.info 'receive_sms from user: ' + from_user + ' ,to app: ' + to_app + ' , body:' + body
+    Rails.logger.info 'receive_sms from user: ' + from_user + ' ,to app: ' + to_app + ' , body:' + body
   end
 
   def lifecycle_notification(channel, invoker, reason, application_name, notification_type, access_tokens)
