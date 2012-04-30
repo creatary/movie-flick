6f365027ccf73b70b4b89bc519549500d241c1e7
Krzysztof Nowicki <knowicki@knowicki-Latitude-E6520.(none)>
2012-04-30 13:19:57 +0200

    add missing transaction_id in handler

--- app/helpers/creatary_handler.rb
+++ app/helpers/creatary_handler.rb
@@ -8,7 +8,8 @@ class CreataryHandler
     # should return the URL to which Creatary should redirect
   end
 
-  def receive_sms(from_user, to_app, body)
+  def receive_sms(from_user, to_app, body, transaction_id)
+    LOGGER.info 'receive_sms from user: ' + from_user + ' ,to app: ' + to_app + ' , body:' + body
   end
 
   def lifecycle_notification(channel, invoker, reason, application_name, notification_type, access_tokens)
