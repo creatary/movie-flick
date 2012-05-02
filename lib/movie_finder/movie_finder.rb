b146d598e8ddd177b0c3ea27375cc8bd10fafe4a
Ewa Waliczek <ewa.waliczek@gmail.com>
2012-05-02 15:21:32 +0200

    list movies for city using google_movies

--- lib/movie_finder/movie_finder.rb
+++ lib/movie_finder/movie_finder.rb
@@ -3,9 +3,27 @@ module MovieFinder
   class MovieFinder
 
     def find(city)
-      [Movie.new("title1", "7.8"),
-       Movie.new("title2", "6.8"),
-       Movie.new("title3", "5.8")]
+      googleMovies = GoogleMovies::Client.new(city)
+      movies = Array.new
+
+      if googleMovies.movies_theaters.size > 0
+        googleMovies.movies_theaters.each do |theater|
+          theater.movies.each do |movie|
+            exists = false
+            movies.each do |m|
+              if movie.name.casecmp(m.name) == 0
+                exists = true
+              end
+            end
+            if !exists
+              movies << movie
+            end
+          end
+        end
+      end
+
+      movies.sort! {|a,b| a.name.downcase <=> b.name.downcase}
+      movies
     end
 
 
