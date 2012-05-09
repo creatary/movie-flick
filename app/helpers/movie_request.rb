2e4cf614c0bd9f9855ad62c074a1958ffe6950fd
Michal Lipski <mlipski@impaq.com.pl>
2012-05-09 13:36:17 +0200

    minor fix

--- app/helpers/movie_request.rb
+++ app/helpers/movie_request.rb
@@ -79,7 +79,7 @@ class MoviesInfoRequest
   end
 
   def display_movie_info(movie)
-    return "Movie was not found" if movie.empty?
+    return "Movie was not found" if movie.nil?
     "#{movie.title}(#{movie.year}) #{movie.critics_consensus}, CS:#{movie.critics_score}, AS: #{movie.audience_score} "
   end
 
