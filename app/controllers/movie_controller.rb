62cf8e18a2fc3bd0f17cfd32ec1dedaa07018807
Ewa Waliczek <ewa.waliczek@gmail.com>
2012-05-14 11:05:56 +0200

    movie controller refactoring

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,15 +1,5 @@
 class MovieController < ApplicationController
 
-  #def get
-  #  movieFinder = MovieFinder::MovieFinder.new
-  #  @movies = movieFinder.find(params[:city])
-  #
-  #  respond_to do |format|
-  #    format.html
-  #    format.json { render :json => @movies }
-  #  end
-  #end
-
   def find_by_location
     movieFinder = MovieFinder::MovieFinder.new
     @movies = movieFinder.find_by_location(params[:city])
